fd4776a80a097a70a46cf2e76a71c75fc72136ad
rr999888tw <rr999888tw@gmail.com>
2018-03-07 11:32:38 +0800

    ss

--- datascience/03_06_1.py
+++ datascience/03_06_1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def sigmoid(x):
 	return 1/(1+np.exp(-x))
 
-def mse(prediction, groundtruth):
+def mseLoss(prediction, groundtruth):
 	return (( prediction - groundtruth )**2 )/2
 
 # z(x,w,b)
@@ -16,37 +16,50 @@ def nn(inp, weight, bias):
 def croEntrLoss(prediction, groundtruth):
 	return -1* (groundtruth* np.log(prediction)+ (1- groundtruth)* np.log(1- prediction))
 
-
-
+def derOfMSELoss(inp, weight, bias):
+	return sigmoid(nn(inp, weight, bias)) * sigmoid(nn(inp, weight, bias)) * (1-sigmoid(nn(inp, weight, bias)))
 
 def derOfCroEntroLoss(inp, weight, bias):
 	return sigmoid(nn(inp, weight, bias))
 
 
+
 # paramaters able to be modified
 x = 1
 y = 0
-learningrate = 0.005
+learningrate = 0.15
 Epochs = 300
 w = 2
 b = 2
 f = []
-n = []
-
+n1 = []
+g = []
+n2 = []
 
 #'''
 for i in range(1, Epochs+1):
 	w -= learningrate * derOfCroEntroLoss(x,w,b)
 	b -= learningrate * derOfCroEntroLoss(x,w,b)
 	f.append(croEntrLoss(sigmoid(nn(x,w,b)), y))
-	n.append(i)
+	n1.append(i)
 #'''
 
-plt.plot(n,f)
+plt.plot(n1,f)
 plt.show()
 
 
 
+w = 2
+b = 2
+
+for i in range(1, Epochs+1):
+	w -= learningrate * derOfMSELoss(x,w,b)
+	b -= learningrate * derOfMSELoss(x,w,b)
+	g.append(mseLoss(sigmoid(nn(x,w,b)), y))
+	n2.append(i)
+
+plt.plot(n2,g)
+plt.show()
 
 
 
