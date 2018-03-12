8d14df58979daa466e42ac583ba9a474d43f4848
rr999888tw <rr999888tw@gmail.com>
2018-03-12 17:49:56 +0800

    ssss

---
+++ Web_Sec_Exp/HW1/spider.py
@@ -0,0 +1,34 @@
+import numpy as np
+import urllib3
+
+http = urllib3.PoolManager()
+
+
+def requestStatus(str1,str2):
+	try:
+		r = http.request('GET', str1+str2)
+		# print (r.status)
+		return (r.status)
+	except:
+		return 0
+		# print("this is shit")
+
+target = 'https://www.ets.org/gre/'
+
+myfile = open("all.txt","r")
+
+
+temp = myfile.read().splitlines()
+# print(temp)
+
+# requestStatus(target,"")
+size = len(temp)
+sucArr = []
+
+for i in range(0, size):
+	ret = requestStatus(target, temp[i])
+	if(ret == 200):
+		sucArr.append(target+temp[i])
+		print(target+temp[i])
+		print(" try " + str(i+1) + " times , success " + str(len(sucArr)) +" times.")
+print(sucArr)
