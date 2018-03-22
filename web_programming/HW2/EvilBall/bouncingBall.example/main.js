ebbdeb07f9c2fb70c7217094d6a372fc973d2d4d
rr999888tw <rr999888tw@gmail.com>
2018-03-22 17:25:05 +0800

    ss

--- web_programming/HW2/EvilBall/bouncingBall.example/main.js
+++ web_programming/HW2/EvilBall/bouncingBall.example/main.js
@@ -6,6 +6,9 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
+var score = document.getElementById("#eaten");
+
+
 // function to generate random number
 
 function random(min,max) {
@@ -19,6 +22,10 @@ var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
 ctx.fillRect(0, 0, width, height);
+
+///////////////////////////////////////////////////////////////////
+// define the object prototype of Ball 							 //
+///////////////////////////////////////////////////////////////////
 
 
 function Ball(x, y, vx, vy, color, r) {
@@ -35,7 +42,6 @@ function Ball(x, y, vx, vy, color, r) {
 
 };
 
-
 Ball.prototype.draw = function() {
 	ctx.beginPath();
 	ctx.fillStyle = this.color;
@@ -50,42 +56,136 @@ Ball.prototype.move = function() {
 	if(this.y-this.r < 0 || this.y+this.r > height) this.vy = -this.vy;
 	this.x += this.vx;
 	this.y += this.vy;
-
-
 };
 
 Ball.prototype.checkCollision = function() {
 
 };
 
+
+///////////////////////////////////////////////////////////////////
+// define the object prototype of EvilBall 						 //
+///////////////////////////////////////////////////////////////////
+
+
+class EvilBall extends Ball{
+
+
+	move(){
+		if(this.x-this.r < 0) this.x = this.r;
+		if(this.x+this.r > width) this.x = width - this.r;
+		if(this.y-this.r < 0)  this.y = this.r;
+		if(this.y+this.r > height) this.y = height - this.r;
+	}
+
+	checkCollision(regBall) {
+		var horizontalD = this.x - regBall.x;
+		var verticalD = this.y - regBall.y;
+		var distance = Math.sqrt(horizontalD**2 + verticalD**2);
+		if(distance < this.r + regBall.r)
+			return true;
+		else 
+			return false;
+	}
+
+	keyboardMove(direction){
+		if(direction == "up"){
+			this.y -= 20;
+		}
+		if(direction == "down"){
+			this.y += 20;
+		}
+		if(direction == "left"){
+			this.x -= 20;
+		}
+		if(direction == "right"){
+			this.x += 20;
+		}
+	}
+
+}
+
+///////////////////////////////////////////////////////////////////
+// define the object prototype of RegBall 						 //
+///////////////////////////////////////////////////////////////////
+
+class RegBall extends Ball{
+
+	// constructor(){
+	// 	this.vx = 0;
+	// 	this.vy = 0;
+	// }
+}
+
+
+
+///////////////////////////////////////////////////////////////////
+// announce the initial RegBalls and EvilBall 				     //
+///////////////////////////////////////////////////////////////////
+
 var ball = new Ball(random(0, width), random(0, height), random(-10, 10), random(-10, 10), "red" , random(10,20));
 var balls = [];
-for (let i = 0; i< 10; ++i){
-	let g = new Ball(random(0, width), random(0, height), random(-10, 10), random(-10, 10),
-	"rgb("+random(150,255)+', '+random(150,255)+', '+random(150,255)+ " )" , random(10,20));
+var evilBall = new EvilBall(width/2, height/2, 0, 0, 'white', 50);
+evilBall.draw();
+balls.push(evilBall);
+
+for(let i= 0; i< 10; ++i){
+	let g = new RegBall(random(0, width), random(0, height), random(-10, 10), random(-10, 10),
+	"rgb("+random(50,255)+', '+random(50,255)+', '+random(50,255)+ " )" , random(10,20));
 	balls.push(g);
 	g.draw();
 }
 
 
 
+
 function loop(){
 	
-	ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+	ctx.fillStyle = "rgba(0, 0, 0, 1)";
 	ctx.fillRect(0, 0, width, height);
-	for(let i= 0; i<10; ++i){
+	for(let i= 0; i< balls.length; ++i){
 		balls[i].draw();
 		balls[i].move();
-
+	}
+	// evilBall.draw();
+	for(let i = 1; i < balls.length; ++i){
+		if(balls[0].checkCollision(balls[i])){
+			balls[i] = new RegBall(random(0, width), random(0, height), random(-10, 10), random(-10, 10),
+			"rgb("+random(150,255)+', '+random(150,255)+', '+random(150,255)+ " )" , random(10,20));
+			score.innerHTML = parseInt(score.innerHTML) + 1;
+		}
 	}
 	requestAnimationFrame(loop);
 }
 
+
+
+document.addEventListener('keydown', function(e) {
+
+    //left
+    if (e.keyCode == 37) {
+        // console.log('start moving LEFT');
+        balls[0].keyboardMove('left');
+        // dx = -1;
+    }
+    else if (e.keyCode == 38) {
+        // console.log('start moving UP');
+        balls[0].keyboardMove('up');
+        // dy = -1;
+    }
+    else if (e.keyCode == 39) {
+        // console.log('start moving RIGHT');
+        balls[0].keyboardMove('right');
+        // dx = 1;
+    }
+    else if (e.keyCode == 40) {
+        // console.log('start moving DOWN');
+        balls[0].keyboardMove('down');
+        // dy = 1;
+    }
+});
+
+
+
 loop();
-
-
-
-
-
-
 
